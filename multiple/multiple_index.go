d7e70354417705fb863645a3d497da3f602c64ed
lezhnev <lezhnev.work@gmail.com>
2023-10-27 14:05:43 +0200

    add read/write support to the IndexDirectory

--- multiple/multiple_index.go
+++ multiple/multiple_index.go
@@ -1,20 +1,194 @@
 package multiple
 
 import (
+	"cmp"
 	"fmt"
+	"github.com/lezhnev74/go-iterators"
+	"github.com/lezhnev74/inverted_index/single"
+	"golang.org/x/exp/constraints"
 	"os"
+	"path"
+	"time"
 )
 
 // IndexDirectory manages multiple index files in a directory.
 // it supports the same read/write API as a single index file
-// and also manages concurrent merging process (removing merged files to keep the disk space low)
-type IndexDirectory struct {
+// and also manages concurrent merging process (removing merged files to keep the disk space low).
+type IndexDirectory[T constraints.Ordered] struct {
 	directoryPath           string
 	currentList, mergedList *filesList
+
+	// index config
+	segmentSize       uint32
+	serializeValues   func([]T) ([]byte, error)
+	unserializeValues func([]byte) ([]T, error)
 }
 
-func NewIndexDirectory(path string) error {
-	// test the directory for read/write permissions.
+type IndexDirectoryWriter[T constraints.Ordered] struct {
+	indexDirectory *IndexDirectory[T]
+	internalWriter single.InvertedIndexWriter[T]
+	filename       string
+}
+
+func (d *IndexDirectory[T]) NewWriter() (single.InvertedIndexWriter[T], error) {
+	w := &IndexDirectoryWriter[T]{
+		indexDirectory: d,
+		filename:       path.Join(d.directoryPath, fmt.Sprintf("%d", time.Now().UnixMicro())), // filename selection
+	}
+
+	var err error
+
+	w.internalWriter, err = single.NewInvertedIndexUnit[T](w.filename, d.segmentSize, d.serializeValues, d.unserializeValues)
+	if err != nil {
+		defer os.Remove(w.filename)
+		return nil, fmt.Errorf("unable to make a new index: %w", err)
+	}
+
+	return w, nil
+}
+
+func (i *IndexDirectoryWriter[T]) Close() error {
+	if err := i.internalWriter.Close(); err != nil {
+		return err
+	}
+
+	// add the file to the active list
+	i.indexDirectory.currentList.safeWrite(func() {
+		i.indexDirectory.currentList.putFile(
+			i.filename,
+			i.internalWriter.(*single.InvertedIndex[T]).Len(),
+		)
+	})
+
+	return nil
+}
+
+func (i *IndexDirectoryWriter[T]) Put(term string, values []T) error {
+	return i.internalWriter.Put(term, values)
+}
+
+type IndexDirectoryReader[T constraints.Ordered] struct {
+	indexDirectory *IndexDirectory[T]
+}
+
+// NewReader makes a new InvertedIndexReader that hides multiple files.
+// A reader multiplex readers from individual files and allows early closing via Close() call.
+func (d *IndexDirectory[T]) NewReader() (*IndexDirectoryReader[T], error) {
+	r := &IndexDirectoryReader[T]{
+		indexDirectory: d,
+	}
+	return r, nil
+}
+
+func (i *IndexDirectoryReader[T]) ReadTerms() (go_iterators.Iterator[string], error) {
+
+	iteratorSelect := func(ii single.InvertedIndexReader[T]) (go_iterators.Iterator[string], error) {
+		return ii.ReadTerms()
+	}
+
+	return joinIterators(
+		i,
+		go_iterators.NewSliceIterator([]string{}),
+		iteratorSelect,
+	)
+}
+
+// joinIterators creates a selection tree from all index files' iterators.
+// it makes sure cleanup is happening when the reading is over.
+func joinIterators[T constraints.Ordered, V constraints.Ordered](
+	i *IndexDirectoryReader[T],
+	iterator go_iterators.Iterator[V],
+	selectIterator func(ii single.InvertedIndexReader[T]) (go_iterators.Iterator[V], error),
+) (go_iterators.Iterator[V], error) {
+	// acquiring index read lock is enough here
+	// as no logic is allowed to remove a file without a write lock acquired
+	fileList := i.indexDirectory.currentList
+	fileList.lock.RLock()
+	defer fileList.lock.RUnlock()
+
+	openIndexes := make(map[*indexFile]single.InvertedIndexReader[T], len(fileList.files))
+
+	for _, indexFile := range fileList.files {
+
+		ii, err := single.OpenInvertedIndex(indexFile.path, i.indexDirectory.unserializeValues)
+		if err != nil {
+			return nil, fmt.Errorf("open index at %s: %w", indexFile.path, err)
+		}
+		openIndexes[indexFile] = ii
+
+		selectedIterator, err := selectIterator(ii)
+		if err != nil {
+			_ = ii.Close()
+			return nil, fmt.Errorf("read segments from %s: %w", indexFile.path, err)
+		}
+
+		indexFile.rlock.RLock()
+
+		// wrap up the iterator to clean up upon closing
+		selectedIterator = go_iterators.NewClosingIterator(selectedIterator, func(innerErr error) (err error) {
+			defer indexFile.rlock.RUnlock() // release the underlying file
+			defer func() {
+				iteratorCloseError := ii.Close()
+				if err != nil {
+					err = iteratorCloseError // report the iterator close error
+				}
+			}()
+
+			if innerErr != nil {
+				return innerErr
+			}
+
+			return
+		})
+
+		iterator = go_iterators.NewUniqueSelectingIterator(iterator, selectedIterator, cmp.Compare[V])
+	}
+
+	return iterator, nil
+}
+
+func (i *IndexDirectoryReader[T]) ReadValues(terms []string, min T, max T) (go_iterators.Iterator[T], error) {
+	iteratorSelect := func(ii single.InvertedIndexReader[T]) (go_iterators.Iterator[T], error) {
+		return ii.ReadValues(terms, min, max)
+	}
+
+	return joinIterators(
+		i,
+		go_iterators.NewSliceIterator([]T{}),
+		iteratorSelect,
+	)
+}
+
+func (i *IndexDirectoryReader[T]) Close() error {
+
+	panic("implement me")
+}
+
+func NewIndexDirectory[T constraints.Ordered](
+	path string,
+	segmentSize uint32,
+	serializeValues func([]T) ([]byte, error),
+	unserializeValues func([]byte) ([]T, error),
+) (*IndexDirectory[T], error) {
+	if err := checkPermissions(path); err != nil {
+		return nil, err
+	}
+
+	index := &IndexDirectory[T]{
+		directoryPath: path,
+		currentList:   NewFilesList(),
+		mergedList:    NewFilesList(),
+
+		segmentSize:       segmentSize,
+		serializeValues:   serializeValues,
+		unserializeValues: unserializeValues,
+	}
+
+	return index, nil
+}
+
+// checkPermissions tests the directory for read/write permissions.
+func checkPermissions(path string) error {
 	s, err := os.Stat(path)
 	if err != nil {
 		return fmt.Errorf("bad index directory: %w", err)
--- multiple/multiple_index_test.go
+++ multiple/multiple_index_test.go
@@ -1,23 +1,120 @@
 package multiple
 
 import (
+	go_iterators "github.com/lezhnev74/go-iterators"
+	"github.com/lezhnev74/inverted_index/single"
 	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
 )
+
+func TestReadWrite(t *testing.T) {
+	type test struct {
+		name    string
+		prepare func(indexDir *IndexDirectory[uint32])
+		assert  func(indexDir *IndexDirectory[uint32], t *testing.T)
+	}
+
+	tests := []test{
+		{
+			name:    "read from empty folder",
+			prepare: func(indexDir *IndexDirectory[uint32]) {},
+			assert: func(indexDir *IndexDirectory[uint32], t *testing.T) {
+				r, err := indexDir.NewReader()
+				require.NoError(t, err)
+
+				// read terms
+				termsIterator, err := r.ReadTerms()
+				require.NoError(t, err)
+
+				terms := go_iterators.ToSlice(termsIterator)
+				require.Empty(t, terms)
+
+				// read values
+				valuesIterator, err := r.ReadValues([]string{"term1", "term2"}, 0, 100)
+				require.NoError(t, err)
+
+				values := go_iterators.ToSlice(valuesIterator)
+				require.Empty(t, values)
+			},
+		},
+		{
+			name: "write and read terms and values",
+			prepare: func(indexDir *IndexDirectory[uint32]) {
+				w, err := indexDir.NewWriter()
+				require.NoError(t, err)
+
+				err = w.(single.InvertedIndexWriter[uint32]).Put("term1", []uint32{1, 2})
+				require.NoError(t, err)
+				err = w.(single.InvertedIndexWriter[uint32]).Put("term2", []uint32{2, 3})
+				require.NoError(t, err)
+				err = w.(single.InvertedIndexWriter[uint32]).Close()
+				require.NoError(t, err)
+			},
+			assert: func(indexDir *IndexDirectory[uint32], t *testing.T) {
+				r, err := indexDir.NewReader()
+				require.NoError(t, err)
+
+				// read terms
+				termsIterator, err := r.ReadTerms()
+				require.NoError(t, err)
+
+				terms := go_iterators.ToSlice(termsIterator)
+				expectedTerms := []string{"term1", "term2"}
+				require.EqualValues(t, expectedTerms, terms)
+
+				// read values
+				valuesIterator, err := r.ReadValues([]string{"term1", "term2"}, 0, 100)
+				require.NoError(t, err)
+
+				values := go_iterators.ToSlice(valuesIterator)
+				expectedValues := []uint32{1, 2, 3}
+				require.EqualValues(t, expectedValues, values)
+
+				// Close
+				require.NoError(t, valuesIterator.Close())
+				require.ErrorIs(t, valuesIterator.Close(), go_iterators.ClosedIterator)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dirPath, _ := os.MkdirTemp("", "")
+			defer os.RemoveAll(dirPath)
+
+			index, err := NewTestIndexDirectory(dirPath)
+			require.NoError(t, err)
+			tt.prepare(index)
+			tt.assert(index, t)
+		})
+	}
+}
 
 func TestItValidatesDirectoryPermissions(t *testing.T) {
 	dirPath, _ := os.MkdirTemp("", "")
 	defer os.RemoveAll(dirPath)
 
 	// valid directory
-	require.NoError(t, NewIndexDirectory(dirPath))
+	_, err := NewTestIndexDirectory(dirPath)
+	require.NoError(t, err)
 
 	// not writable permissions
 	require.NoError(t, os.Chmod(dirPath, 0400))
-	require.ErrorContains(t, NewIndexDirectory(dirPath), "the directory is not writable")
+	_, err = NewTestIndexDirectory(dirPath)
+	require.ErrorContains(t, err, "the directory is not writable")
 
 	// not writable permissions
 	require.NoError(t, os.Chmod(dirPath, 0200))
-	require.ErrorContains(t, NewIndexDirectory(dirPath), "the directory is not readable")
+	_, err = NewTestIndexDirectory(dirPath)
+	require.ErrorContains(t, err, "the directory is not readable")
+}
+
+func NewTestIndexDirectory(path string) (*IndexDirectory[uint32], error) {
+	return NewIndexDirectory[uint32](
+		path,
+		1000,
+		single.CompressUint32,
+		single.DecompressUint32,
+	)
 }
